948c2645e3be2b6fdcc1d0a5006b1853ff5bd8db
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2019-05-19 22:06:00 +0300

    Complete QEMU script

---
+++ scripts/qemu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+XVISOR=xvisor
+
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+# Copy FreeRTOS source
+cd $DIRECTORY/../$XVISOR
+qemu-system-arm -M vexpress-a9 -m 512M -nographic \
+	-kernel build/vmm.bin \
+	-dtb    build/arch/arm/board/generic/dts/arm/vexpress-v2p-ca9.dtb \
+	-initrd build/disk.img
